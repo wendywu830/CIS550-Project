e5bd240a19f8312fa68a9f3543a8697bdaa4d26d
alzhang99 <54654911+alzhang99@users.noreply.github.com>
2020-05-04 17:24:45 -0400

    optimized food

--- server/routes/flightRoutes.js
+++ server/routes/flightRoutes.js
@@ -216,6 +216,7 @@ function searchLayoverCat(req, res) {
     ON r.target_id = a2.id
     WHERE a2.city = :dest_city
     ),
+
     biz AS (
       SELECT name, city, stars, business_id
       FROM business b
@@ -254,6 +255,185 @@ function searchLayoverCat(req, res) {
     }
   });
 }
+
+/**
+ * search best states and country to go to for foodies with direct flight to 
+ * dest from src
+ * states and country of dest and the number of businesses there with label 
+ * restaurant or food with stats >= 3
+ * @param source_city Source City
+ */
+function searchFoodDest(req, res) {
+  var query = `
+    WITH 
+
+    srcID AS (
+    SELECT id
+    FROM airports
+    WHERE city = :source_city),
+    
+    srcRoutes AS (
+    SELECT routes.target_id
+    FROM srcID
+    JOIN routes
+    ON routes.source_id = srcID.id
+    ),
+    
+    allRoutes AS
+    (SELECT dest.country, dest.lat, dest.lon
+    FROM srcRoutes r
+    JOIN airports dest
+    ON r.target_id = dest.id),
+    
+    busCity AS 
+    (SELECT city as bc_city, state, lat, lon, business_id
+    FROM business
+    WHERE categories LIKE '%Restaurants%'
+    OR categories LIKE '%Food%'
+    AND stars >= 4),
+    
+    busState AS 
+    (SELECT state, COUNT(business_id) as numBusinesses
+    FROM busCity
+    GROUP BY state),
+    
+    res AS
+    (SELECT busCity.bc_city, busCity.state, busState.numBusinesses, allRoutes.country
+    FROM allRoutes
+    JOIN busCity
+    ON (allRoutes.lat = busCity.lat AND allRoutes.lon = busCity.lon)
+    JOIN busState
+    ON busCity.state = busState.state
+    WHERE numBusinesses > 50
+    ORDER BY numBusinesses DESC)
+    
+    SELECT DISTINCT(res.state), res.numBusinesses, res.country
+    FROM res
+    ORDER BY numBusinesses DESC
+  
+  `;
+  let source_city = req.params.source_city;
+  const binds = [source_city];
+
+  oracledb.getConnection({
+    user : credentials.user,
+    password : credentials.password,
+    connectString : credentials.connectString
+  }, function(err, connection) {
+    if (err) {
+      console.log(err);
+    } else {
+      connection.execute(query, binds, function(err, result) {
+        if (err) {console.log(err);}
+        else {
+          console.log(result.rows)
+          res.json(result.rows)
+        }
+      });
+    }
+  });
+}
+
+/**
+ * search 5 best businesses, states and country to go from src
+ * algo uses best travel categories and finds the number of businesses 
+ * in each state- the businesses have stars >= 4 and each given suggestion 
+ * state has at least 50 of these fun businesses
+ * there also exists a direct flight from source to these destinations
+ * randomly sorts the results and picks top 5
+ * @param source_city Source City
+ */
+function searchMysteryDest(req, res) {
+  var query = `
+    SELECT * 
+    FROM
+
+      (WITH 
+      airpSrc AS
+      (SELECT ap.name, ap.id, ap.country, rt.route_id
+      FROM airports ap
+      JOIN Routes rt
+      ON ap.city = :source_city
+      AND rt.source_id = ap.id
+      ),
+      
+      bus AS
+      (SELECT name, city, state, lat, lon, business_id, stars
+      FROM business
+      WHERE categories LIKE '%Nightlife%'
+      OR categories LIKE '%Beauty and Spas%'
+      OR categories LIKE '%Bakeries%'
+      OR categories LIKE '%Bars%'
+      OR categories LIKE '%Lounges%'
+      OR categories LIKE '%Active Life%'
+      OR categories LIKE '%Breakfast and Brunch%'
+      OR categories LIKE '%Recreation Centers%'
+      OR categories LIKE '%Breweries%'
+      OR categories LIKE '%Shopping%'
+      AND stars >= 4),
+      
+      groupState AS
+      (SELECT state, COUNT(business_id) as count
+      FROM bus
+      GROUP BY state
+      ORDER BY count DESC, state),
+      
+      airpDest AS
+      (SELECT a.id, a.name, a.city, a.country, a.lat, a.lon
+      FROM airports a
+      ),
+      
+      allTab AS
+      (SELECT DISTINCT(bus.city), groupState.state, bus.name AS bus_name, bus.stars, bus.business_id, groupState.count, 
+      airpDest.name AS airport, airpDest.country, airpDest.id
+      FROM bus
+      JOIN groupState
+      ON bus.state = groupState.state
+      JOIN airpDest
+      ON airpDest.lat = bus.lat AND airpDest.lon = bus.lon 
+      
+      ),
+      
+      distTabl AS (
+      SELECT DISTINCT(allTab.city) AS bus_city, allTab.state AS bus_state, allTab.bus_name, allTab.stars, allTab.count, allTab.airport, 
+      allTab.country, allTab.business_id
+      FROM allTab
+      JOIN Routes r1
+      ON r1.target_ID = allTab.ID
+      JOIN airpSRC
+      ON r1.source_id = airpSrc.id
+      WHERE count > 50
+      )
+      
+      SELECT DISTINCT(distTabl.bus_state), distTabl.bus_name, distTabl.country
+      FROM distTabl
+      ORDER BY dbms_random.value)
+    
+    WHERE ROWNUM <= 5
+  `;
+  let source_city = req.params.source_city;
+  const binds = [source_city];
+
+  oracledb.getConnection({
+    user : credentials.user,
+    password : credentials.password,
+    connectString : credentials.connectString
+  }, function(err, connection) {
+    if (err) {
+      console.log(err);
+    } else {
+      connection.execute(query, binds, function(err, result) {
+        if (err) {console.log(err);}
+        else {
+          console.log(result.rows)
+          res.json(result.rows)
+        }
+      });
+    }
+  });
+}
+
+
 
 /***********
  * Exports *
